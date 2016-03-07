5e71d72788e2772d87a913c4eb4a55d97aaf69c7
Jochen Klar <jklar@aip.de>
2016-03-07 17:43:02 +0100

    login url and django version

--- daiquiri_app/urls.py
+++ daiquiri_app/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+from daiquiri_auth.views import login
+
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/', login, name='login'),
     url(r'^admin/', include(admin.site.urls)),
 ]
