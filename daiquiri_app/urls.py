366aadf2a6c31a0734d3d8b207f2dcaa27708d38
Jochen Klar <jklar@aip.de>
2016-03-11 17:40:33 +0100

    urls updated

--- daiquiri_app/urls.py
+++ daiquiri_app/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+
+from daiquiri_core.views import home
+from daiquiri_auth.views import login, logout
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
+    url(r'^$', home, name='home'),
     url(r'^auth/', include('daiquiri_auth.urls')),
-    url(r'^%s/' % settings.LOGIN_URL.strip('/'), 'daiquiri_auth.views.login', name='login'),
-    url(r'^%s/' % settings.LOGOUT_URL.strip('/'), 'daiquiri_auth.views.logout', name='logout'),
+    url(r'^%s/' % settings.LOGIN_URL.strip('/'), login, name='login'),
+    url(r'^%s/' % settings.LOGOUT_URL.strip('/'), logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 ]
