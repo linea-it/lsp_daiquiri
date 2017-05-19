14b994e07725deedf6225515ec657b63885970ce
Jochen Klar <jklar@aip.de>
2017-05-19 13:02:58 +0200

    refactor settings

--- daiquiri_app/settings/__init__.py
+++ daiquiri_app/settings/__init__.py
@@ -1,1 +1,189 @@
+'''
+This files contains all the settings which should be the same in all daiquiri installations.
+Project specific settings should be in base.py
+Server specific settings should be in local.py, which is git-excluded.
+'''
+
+import os
+
+from django.utils.translation import ugettext_lazy as _
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+'''
+Basic Django configuration
+'''
+
+SITE_ID = 1
+
+HTTPS = False
+
+SECRET_KEY = 'this is not a very secret key'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost']
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+INSTALLED_APPS = [
+    # django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    # 3rd party modules
+    'rest_framework',
+    'markdown',
+    'compressor',
+    'djangobower',
+    'widget_tweaks',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware'
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django_settings_export.settings_export',
+            ],
+        },
+    },
+]
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/'),
+)
+
+LANGUAGES = (
+    ('en', _('English')),
+)
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/accounts/logout/'
+
+'''
+Default database connection
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+'''
+Media and static files
+'''
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+    'djangobower.finders.BowerFinder',
+)
+
+'''
+Configuration for django-allauth
+'''
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+ACCOUNT_ADAPTER = 'daiquiri_auth.adapter.DaiquiriAccountAdapter'
+ACCOUNT_SIGNUP_FORM_CLASS = 'daiquiri_auth.forms.SignupForm'
+ACCOUNT_USER_DISPLAY = 'daiquiri_auth.utils.get_full_name'
+
+'''
+Configuration for django-bower
+'''
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'bower_root/')
+
+BOWER_INSTALLED_APPS = (
+    'angular#1.5.8',
+    'angular-resource#1.5.8',
+    'bootstrap#3.3.7',
+    'ngInfiniteScroll#1.2.0',
+    'codemirror#~5.18.2',
+    'components-font-awesome#~4.6.3',
+    'moment#~2.14.1',
+    'angular-file-saver'
+)
+
+'''
+Configuration for django-settings-export
+'''
+
+SETTINGS_EXPORT = [
+    'LOGIN_URL',
+    'LOGOUT_URL',
+    'QUERY'
+]
+
+'''
+Default email setting
+'''
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_FROM = 'info@example.com'
+
+# include settings from base.py
 from .base import *
+
+# include settings from local.py
+from .local import *
--- daiquiri_app/settings/base.py
+++ daiquiri_app/settings/base.py
@@ -1,31 +1,8 @@
 import os
 
-from django.utils.translation import ugettext_lazy as _
+from . import BASE_DIR, INSTALLED_APPS
 
-SITE_ID = 1
-
-HTTPS = False
-
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(PROJECT_DIR)
-
-SECRET_KEY = 'this is not a very secret key'
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['localhost']
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    # daiquiri apps
+INSTALLED_APPS += [
     'daiquiri_auth',
     'daiquiri_contact',
     'daiquiri_core',
@@ -34,64 +11,19 @@ INSTALLED_APPS = [
     'daiquiri_metadata',
     'daiquiri_query',
     'daiquiri_serve',
-    # 3rd party modules
-    'rest_framework',
-    'markdown',
-    'compressor',
-    'djangobower',
-    'widget_tweaks',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-]
-
-MIDDLEWARE_CLASSES = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sites.middleware.CurrentSiteMiddleware'
 ]
 
 ROOT_URLCONF = 'daiquiri_app.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, 'templates/')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django_settings_export.settings_export',
-            ],
-        },
-    },
-]
+WSGI_APPLICATION = 'daiquiri_app.wsgi.application'
 
-COMPRESS_PRECOMPILERS = (
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 7
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-)
-
-
-SETTINGS_EXPORT = [
-    'LOGIN_URL',
-    'LOGOUT_URL',
-    'QUERY'
-]
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+ACCOUNT_PASSWORD_MIN_LENGTH = 4
 
 LOGGING_DIR = os.path.join(BASE_DIR, 'log')
 LOGGING = {
@@ -137,83 +69,6 @@ LOGGING = {
     }
 }
 
-
-ACCOUNT_ADAPTER = 'daiquiri_auth.adapter.DaiquiriAccountAdapter'
-ACCOUNT_SIGNUP_FORM_CLASS = 'daiquiri_auth.forms.SignupForm'
-ACCOUNT_USER_DISPLAY = 'daiquiri_auth.utils.get_full_name'
-
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 7
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
-
-ACCOUNT_USERNAME_MIN_LENGTH = 4
-ACCOUNT_PASSWORD_MIN_LENGTH = 4
-
-WSGI_APPLICATION = 'daiquiri_app.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale/'),
-)
-
-LANGUAGES = (
-    ('en', _('English')),
-)
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_URL = '/accounts/logout/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-    'djangobower.finders.BowerFinder',
-)
-
-BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'bower_root/')
-
-BOWER_INSTALLED_APPS = (
-    'angular#1.5.8',
-    'angular-resource#1.5.8',
-    'bootstrap#3.3.7',
-    'ngInfiniteScroll#1.2.0',
-    'codemirror#~5.18.2',
-    'components-font-awesome#~4.6.3',
-    'moment#~2.14.1',
-    'angular-file-saver'
-)
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_FROM = 'info@example.com'
-
 AUTH = {
     'detail_keys': [
         {
@@ -243,7 +98,6 @@ AUTH = {
         }
     ]
 }
-
 
 QUERY = {
     'async': False,
@@ -326,14 +180,3 @@ QUERY = {
         }
     ]
 }
-
-# try to override with local configuration
-try:
-    from .local import *
-except ImportError:
-    pass
-
-try:
-    INSTALLED_APPS = INSTALLED_APPS + ADDITIONAL_APPS
-except NameError:
-    pass
