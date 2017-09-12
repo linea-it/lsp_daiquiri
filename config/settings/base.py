b0a5c49c54f2f0d3d0870dfd1c6d3f4650139f00
Jochen Klar <jklar@aip.de>
2017-09-12 18:22:39 +0200

    update to current version of Daiquiri

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,35 +24,33 @@ ACCOUNT_PASSWORD_MIN_LENGTH = 4
 ADAPTER_DATABASE = 'daiquiri.core.adapter.database.mariadb.MariaDBAdapter'
 ADAPTER_DOWNLOAD = 'daiquiri.core.adapter.download.mysqldump.MysqldumpAdapter'
 
-AUTH = {
-    'detail_keys': [
-        {
-            'key': 'gender',
-            'label': 'Gender',
-            'data_type': 'radio',
-            'help_text': 'The gender you identify with.',
-            'options': [
-                {
-                    'id': 'female',
-                    'label': 'Female'
-                },
-                {
-                    'id': 'male',
-                    'label': 'Male'
-                },
-                {
-                    'id': 'other',
-                    'label': 'Other'
-                },
-                {
-                    'id': 'prefer_not_to_say',
-                    'label': 'Prefer not to say'
-                }
-            ],
-            'required': True
-        }
-    ]
-}
+AUTH_DETAIL_KEYS = [
+    {
+        'key': 'gender',
+        'label': 'Gender',
+        'data_type': 'radio',
+        'help_text': 'The gender you identify with.',
+        'options': [
+            {
+                'id': 'female',
+                'label': 'Female'
+            },
+            {
+                'id': 'male',
+                'label': 'Male'
+            },
+            {
+                'id': 'other',
+                'label': 'Other'
+            },
+            {
+                'id': 'prefer_not_to_say',
+                'label': 'Prefer not to say'
+            }
+        ],
+        'required': True
+    }
+]
 
 QUERY_QUOTA = {
     'user': '1Gb'
@@ -70,5 +68,22 @@ QUERY_QUEUES = [
         'label': '10 Minutes',
         'timeout': 600,
         'priority': 3
+    }
+]
+
+QUERY_LANGUAGES = [
+    {
+        'key': 'mariadb',
+        'version': 10.1,
+        'label': 'MariaDB SQL',
+        'description': '',
+        'quote_char': '`'
+    },
+    {
+        'key': 'adql',
+        'version': 2.0,
+        'label': 'ADQL',
+        'description': '',
+        'quote_char': '"'
     }
 ]
