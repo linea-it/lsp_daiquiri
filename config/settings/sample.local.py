30ae21421ffb368bcd9ae0603cb8a2089a862fb5
Jochen Klar <jochenklar@gmail.com>
2017-09-18 20:29:16 +0200

    update sample.local.py

--- config/settings/sample.local.py
+++ config/settings/sample.local.py
@@ -1,38 +1,67 @@
+'''
+Secret key, use something random in production
+'''
 SECRET_KEY = 'this is not a very secret key'
 
-DEBUG = False
+'''
+Debug mode, don't use this in production
+'''
+DEBUG = True
 
+'''
+Use Celery to run tasks asyncronous
+'''
 ASYNC = False
 
+'''
+The list of URLs und which this application available
+'''
 ALLOWED_HOSTS = ['localhost', 'ip6-localhost', '127.0.0.1', '[::1]']
 
+'''
+The database connection to be used, see also:
+http://rdmo.readthedocs.io/en/latest/configuration/databases.html
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'daiquiri_app',
         'USER': 'daiquiri_app',
-        'PASSWORD': 'daiquiri_app'
+        'PASSWORD': 'daiquiri_app',
+        'HOST': '',
+        'PORT': ''
     },
     'tap': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'TAP_SCHEMA',
         'USER': 'daiquiri_tap',
         'PASSWORD': 'daiquiri_tap',
-        'HOST': ''
+        'HOST': '',
+        'PORT': ''
     },
     'data': {
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'daiquiri_data',
         'PASSWORD': 'daiquiri_data',
-        'HOST': ''
+        'HOST': '',
+        'PORT': ''
     }
 }
 
+'''
+Base URL Path to this application, i.e. /path for http://exaple.com/path/
+'''
 # BASE_URL = ''
 
+'''
+Use a custom registration workflow
+'''
 # AUTH_WORKFLOW = 'confirmation'
 # AUTH_WORKFLOW = 'activation'
 
+'''
+E-Mail configuration
+'''
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'localhost'
 # EMAIL_PORT = '25'
@@ -40,4 +69,8 @@ DATABASES = {
 # EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
 
+'''
+E-Mail configuration
+'''
 # SENDFILE_BACKEND = 'sendfile.backends.development'
+# SENDFILE_BACKEND = 'sendfile.backends.xsendfile'
