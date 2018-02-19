8c307d408796295696b125d91f40bf4ea3e2300f
Jochen Klar <jklar@aip.de>
2018-02-19 12:06:19 +0100

    cleanup settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,45 +19,10 @@ INSTALLED_APPS = []
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 ACCOUNT_PASSWORD_MIN_LENGTH = 4
 
-ADAPTER_DATABASE = 'daiquiri.core.adapter.database.mariadb.MariaDBAdapter'
-ADAPTER_DOWNLOAD = 'daiquiri.core.adapter.download.mysqldump.MysqldumpAdapter'
-
 AUTH_DETAIL_KEYS = []
 
 QUERY_QUOTA = {
     'user': '1Gb'
 }
-
-QUERY_QUEUES = [
-    {
-        'key': '30s',
-        'label': '30 Seconds',
-        'timeout': 30,
-        'priority': 5
-    },
-    {
-        'key': '10m',
-        'label': '10 Minutes',
-        'timeout': 600,
-        'priority': 3
-    }
-]
-
-QUERY_LANGUAGES = [
-    {
-        'key': 'mariadb',
-        'version': 10.1,
-        'label': 'MariaDB SQL',
-        'description': '',
-        'quote_char': '`'
-    },
-    {
-        'key': 'adql',
-        'version': 2.0,
-        'label': 'ADQL',
-        'description': '',
-        'quote_char': '"'
-    }
-]
 
 VENDOR_CDN = False
