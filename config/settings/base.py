725ee6f17ea36093ac69a9d057484c077754f8ff
Jochen Klar <mail@jochenklar.de>
2019-12-16 11:24:40 +0100

    Update base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,11 +1,18 @@
 import os
-from . import BASE_DIR, DJANGO_APPS, ADDITIONAL_APPS
+
+from . import ADDITIONAL_APPS, BASE_DIR, DJANGO_APPS
 
 SITE_IDENTIFIER = 'example.com'
 SITE_TITLE = 'example.com'
 SITE_DESCRIPTION = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
 SITE_LICENSE = 'CC0'
 SITE_CREATOR = 'Anna Admin'
+SITE_CONTACT = {
+    'name': 'Anna Admin',
+    'address': 'Example Road 1',
+    'email': 'admin@example.com',
+    'telephone': '+01 234 56789'
+}
 SITE_PUBLISHER = 'At vero eos et accusam'
 SITE_CREATED = '2019-01-01'
 SITE_UPDATED = '2019-04-01'
