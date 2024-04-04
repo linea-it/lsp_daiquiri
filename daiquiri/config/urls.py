d672ca708094d254540809c65ecead441cf67305
glaubervila <glauber.vila.verde@gmail.com>
2024-04-04 14:44:13 +0000

    Fixed lint

--- daiquiri/config/urls.py
+++ daiquiri/config/urls.py
@@ -1,7 +1,8 @@
-from daiquiri.core.views import home
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+
+from daiquiri.core.views import home
 
 urlpatterns = [
     path("", home, name="home"),
